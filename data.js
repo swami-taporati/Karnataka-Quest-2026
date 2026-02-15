67fd48561fde974776dba37197d5ed59c4eb2f17
swami-taporati <64623355+swami-taporati@users.noreply.github.com>
2026-02-15 18:02:07 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+const teams = {
+  team1: { name: "Team 1", pin: "1111", score: 0 },
+  team2: { name: "Team 2", pin: "2222", score: 0 },
+  team3: { name: "Team 3", pin: "3333", score: 0 },
+  team4: { name: "Team 4", pin: "4444", score: 0 },
+  team5: { name: "Team 5", pin: "5555", score: 0 }
+};
